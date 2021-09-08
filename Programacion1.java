d01543ab75f1fb8465f56aabf6b82a2b9d43ec05
Wisan0346 <89869338+Wisan0346@users.noreply.github.com>
2021-09-08 14:02:40 -0500

    Add files via upload

---
+++ Programacion1.java
@@ -0,0 +1,26 @@
+
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+        int numero1 =12;
+        int numero2 = 2;
+        double division;
+        int suma, resta, multiplicación;
+        double modulo;
+        double operacion;
+        operacion = 9/3*5-3+8*2;
+        System.out.println("El resultado de la operación es: "+opercacion);
+        division = numero1/numero2;
+        System.out.println("El resultado de la operación es: "+division);
+        suma = numero1+numero2;
+        System.out.println("El resultado de la operación es: "+suma );
+        resta = numero1-numero2;
+        System.out.println("El resultado de la operación es: "+resta);
+        multiplicacion = numero1*numero2;
+        System.out.println("El resultado de la operación es: "+multiplicacion);
+        modulo = numero1%numero2;
+        System.out.println("El resultado de la operación es: "+modulo);
+        
+    }
+    
+}
